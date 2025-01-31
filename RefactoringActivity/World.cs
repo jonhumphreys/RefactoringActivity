e2302d5c6d06ba945940324ed6a52c1f8bb95754
tannerplatt <tplatt@chapman.edu>
2025-01-30 21:22:56 -0800

    Refactored world, ready for review

--- RefactoringActivity/World.cs
+++ RefactoringActivity/World.cs
@@ -3,6 +3,9 @@
 public class World
 {
     public Dictionary<string, Location> Locations;
+    private Location start;
+    private Location forest;
+    private Location cave;
 
     public World()
     {
@@ -12,27 +15,45 @@ public class World
 
     private void InitializeWorld()
     {
-        Location start = new("You are at the starting point of your adventure.");
-        Location forest = new("You are in a dense, dark forest.");
-        Location cave = new("You see a dark, ominous cave.");
-
-        start.Exits.Add("north", "Forest");
-        forest.Exits.Add("south", "Start");
-        forest.Exits.Add("east", "Cave");
-        cave.Exits.Add("west", "Forest");
-
-        start.Items.Add("map");
-        forest.Items.Add("key");
-        forest.Items.Add("potion");
-        cave.Items.Add("sword");
+        InitializeLocations();
+        AddExits();
+        AddItems();
 
         start.Puzzles.Add(new Puzzle("riddle",
             "What's tall as a house, round as a cup, and all the king's horses can't draw it up?", "well"));
 
+        AddLocations();
+    }
+
+    private void InitializeLocations()
+    {
+        start = new("You are at the starting point of your adventure.");
+        forest = new("You are in a dense, dark forest.");
+        cave = new("You see a dark, ominous cave.");
+    }
+
+    private void AddExits()
+    {
+        start.Exits.Add("north", "Forest");
+        forest.Exits.Add("south", "Start");
+        forest.Exits.Add("east", "Cave");
+        cave.Exits.Add("west", "Forest");
+    }
+
+    private void AddItems()
+    {
+        start.Items.Add("map");
+        forest.Items.Add("key");
+        forest.Items.Add("potion");
+        cave.Items.Add("sword");
+    }
+
+    private void AddLocations()
+    {
         Locations.Add("Start", start);
         Locations.Add("Forest", forest);
         Locations.Add("Cave", cave);
-    }
+    } 
 
     public bool MovePlayer(Player player, string direction)
     {
@@ -60,14 +81,34 @@ public class World
         Location location = Locations[locationName];
         string details = location.Description;
         
-        if (location.Exits.Count > 0)
+        details = Details(location, details);
+
+        return details;
+    }
+
+    private static string Details(Location location, string details)
+    {
+        details = AddExitDetails(location, details);
+        details = AddItemsDetails(location, details);
+        details = AddPuzzleDetails(location, details);
+
+        return details;
+    }
+
+    private static string AddPuzzleDetails(Location location, string details)
+    {
+        if (location.Puzzles.Count > 0)
         {
-            details += " Exits lead: ";
-            foreach (string exit in location.Exits.Keys)
-                details += exit + ", ";
-            details = details.Substring(0, details.Length - 2);
+            details += "\nYou see the following puzzles:";
+            foreach (Puzzle puzzle in location.Puzzles) 
+                details += $"\n- {puzzle.Name}";
         }
 
+        return details;
+    }
+
+    private static string AddItemsDetails(Location location, string details)
+    {
         if (location.Items.Count > 0)
         {
             details += "\nYou see the following items:";
@@ -75,11 +116,17 @@ public class World
                 details += $"\n- {item}";
         }
 
-        if (location.Puzzles.Count > 0)
+        return details;
+    }
+
+    private static string AddExitDetails(Location location, string details)
+    {
+        if (location.Exits.Count > 0)
         {
-            details += "\nYou see the following puzzles:";
-            foreach (Puzzle puzzle in location.Puzzles) 
-                details += $"\n- {puzzle.Name}";
+            details += " Exits lead: ";
+            foreach (string exit in location.Exits.Keys)
+                details += exit + ", ";
+            details = details.Substring(0, details.Length - 2);
         }
 
         return details;
